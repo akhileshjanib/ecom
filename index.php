286be57d10769f6b364f42d38c99bd2df4bbabf8
akhileshjanib <53337883+akhileshjanib@users.noreply.github.com>
2019-09-30 13:33:53 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>ecommerce</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="views/layout/css/style.css" />
+    </head>
+    <body>
+        
+    </body>
+</html>
+<?php
+    include('views/layout/navbar.php')
+?>
+<?php
+    include('views/layout/products.php')
+?>
